fc09e7ea214ea13d3849bbaa3123585ef31a8d4a
Moritisimor <moritzgierschner@protonmail.com>
2025-04-19 10:03:33 +0200

    Put Java Files into a folder

---
+++ JavaProgramme/Katze.java
@@ -0,0 +1,16 @@
+public class Katze {
+	public static void Geraeusch() {
+		System.out.println("Miau!");
+		}
+	public static void Farbe() {
+		System.out.println("Schwarz");
+		}
+	public static void AnzahlPfoten() {
+		System.out.println(4);
+		}
+	public static void main(String[] args) {
+		Geraeusch();
+		Farbe();
+		AnzahlPfoten();
+		}
+}
---
+++ JavaProgramme/Maus.java
@@ -0,0 +1,9 @@
+public class Maus {
+	static void quieken() {
+		System.out.println("quiek!");
+		}
+
+	public static void main(String[] args) {
+		quieken();
+	}
+}
