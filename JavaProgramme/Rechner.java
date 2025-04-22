802b793a4d306f6e6532dd52959285fe700b71d8
Moritisimor <moritzgierschner@protonmail.com>
2025-04-22 12:57:26 +0200

    improved rechner's error handling

--- JavaProgramme/Rechner.java
+++ JavaProgramme/Rechner.java
@@ -2,43 +2,46 @@ import java.util.Scanner;
 
 public class Rechner {
     public static void main(String[] args) {
-
+        try {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Bitte geben Sie Ihre erste Zahl an: ");
-        String mystring1 = sc.nextLine();
+            System.out.print("Bitte geben Sie Ihre erste Zahl an: ");
+            String mystring1 = sc.nextLine();
 
-        System.out.print("Bitte geben Sie Ihre zweite Zahl an: ");
-        String mystring2 = sc.nextLine();
+            System.out.print("Bitte geben Sie Ihre zweite Zahl an: ");
+            String mystring2 = sc.nextLine();
 
-        System.out.print("Bitte geben Sie Ihren Operator an: ");
-        String operator = sc.nextLine();
+            System.out.print("Bitte geben Sie Ihren Operator an: ");
+            String operator = sc.nextLine();
 
-        Float x = Float.parseFloat(mystring1);
-        Float y = Float.parseFloat(mystring2);
+            Float x = Float.parseFloat(mystring1);
+            Float y = Float.parseFloat(mystring2);
 
-        if (operator.equals("+")) {
-        System.out.print("Ihr Ergebnis ist: ");
-        System.out.println(x + y); }
+            if (operator.equals("+")) {
+            System.out.print("Ihr Ergebnis ist: ");
+            System.out.println(x + y); }
 
-        else if (operator.equals("-")) {
-        System.out.print("Ihr Ergebnis ist: ");
-        System.out.println(x - y); }
+            else if (operator.equals("-")) {
+            System.out.print("Ihr Ergebnis ist: ");
+            System.out.println(x - y); }
 
-        else if (operator.equals("*")) {
-        System.out.print("Ihr Ergebnis ist: ");
-        System.out.println(x * y); }
+            else if (operator.equals("*")) {
+            System.out.print("Ihr Ergebnis ist: ");
+            System.out.println(x * y); }
 
-        else if (operator.equals("/") && y == 0) {
-        System.out.println("Kann nicht mit null dividieren."); }
+            else if (operator.equals("/") && y == 0) {
+            System.out.println("Kann nicht mit null dividieren."); }
 
-        else if (operator.equals("/")) {
-        System.out.print("Ihr Ergebnis ist: ");
-        System.out.println(x / y); }
+            else if (operator.equals("/")) {
+            System.out.print("Ihr Ergebnis ist: ");
+            System.out.println(x / y); }
 
-        else {
-        System.out.println("Bitte geben Sie einen gültigen Operator an."); }
+            else {
+            System.out.println("Bitte geben Sie einen gültigen Operator an."); }
 
-        sc.close();
+        sc.close(); }
+        catch(NumberFormatException e) {
+            System.out.println("Bitte geben Sie eine gültige Zahl an."); }    
+        
     }
 }
