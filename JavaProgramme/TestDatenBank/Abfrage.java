34b39231f7404a4241b9f4f31a66ec250cbeeeff
Moritisimor <moritzgierschner@protonmail.com>
2025-04-24 18:24:27 +0200

    Added A Register
    Person objects are now stored on a different register file

--- JavaProgramme/TestDatenBank/Abfrage.java
+++ JavaProgramme/TestDatenBank/Abfrage.java
@@ -3,21 +3,18 @@ import java.util.Scanner;
 
 public class Abfrage {
     public static void main(String[] args) {
-        //Personenspeicheranfang
-        Person maxmustermann1 = new Person("Max", "Mustermann", "Musterstraße 123", "Postleitzahl", "123456789");
-        Person hanswurst1 = new Person("Hans", "Wurst", "Wurststraße", "54321", "987654321");
-        //Personenspeicherende
-
         System.out.println("Folgende Daten sind momentan verfügbar:");
-        System.out.println("Vorname");
-        System.out.println("Nachname");
-        System.out.println("Adresse");
-        System.out.println("Postleitzahl");
-        System.out.println("Telefonnummer");
+        System.out.println("-" + "Vorname ");
+        System.out.println("-" + "Nachname");
+        System.out.println("-" + "Adresse ");
+        System.out.println("-" + "Postleitzahl");
+        System.out.println("-" + "Telefonnummer");
 
         System.out.println(" ");
         System.out.println("Und folgende Personen: ");
-        System.out.println("maxmustermann1");
+        System.out.println("-" + "maxmustermann1");
+        System.out.println("-" + "hanswurst1");
+        System.out.println("-" + "erikablume1");
 
         System.out.print("Geben Sie an, über welche Person Sie Daten abfragen möchten: ");
 
@@ -26,11 +23,19 @@ public class Abfrage {
         String abfrage = sc.nextLine();
 
         if (abfrage.equals("maxmustermann1")) {
-            maxmustermann1.Datenanzeige();
+            PersonenRegister.maxmustermann1.Datenanzeige();
         }
 
         else if (abfrage.equals("hanswurst1")) {
-            hanswurst1.Datenanzeige();
+            PersonenRegister.hanswurst1.Datenanzeige();
+        }
+        
+        else if (abfrage.equals("erikablume1")) {
+            PersonenRegister.erikablume1.Datenanzeige();
+        }
+
+        else {
+            System.out.println("Diese Person ist in der Datenbank nicht vorhanden.");
         }
 
         sc.close();
---
+++ JavaProgramme/TestDatenBank/PersonenRegister.java
@@ -0,0 +1,7 @@
+package TestDatenBank;
+
+public class PersonenRegister {
+    public static Person maxmustermann1 = new Person("Max", "Mustermann", "Musterstraße 123", "Postleitzahl", "123456789");
+    public static Person hanswurst1 = new Person("Hans", "Wurst", "Wurststraße", "54321", "987654321");
+    public static Person erikablume1 = new Person("Erika", "Blume", "Blumstraße 69", "32154", "192837465");
+}
