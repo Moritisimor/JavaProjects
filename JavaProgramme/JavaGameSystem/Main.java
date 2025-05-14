b2d1e14e37261005c02dd2e5728d711a53e45531
Moritisimor <moritzgierschner@protonmail.com>
2025-05-14 18:52:41 +0200

    I changed my mind.
    Made a functioning program for setting player data along with simplification of player class

--- JavaProgramme/JavaGameSystem/Main.java
+++ JavaProgramme/JavaGameSystem/Main.java
@@ -1,5 +1,6 @@
 package JavaGameSystem;
 
+import java.util.List;
 import java.util.Scanner;
 import JavaGameSystem.Player.Player;
 
@@ -7,27 +8,53 @@ public class Main {
     public static void main(String[] args) {
         // Instance Initializing Region
 
-        Player Markus = new Player("Markus", "none", 150, 50, 100, 50);
+        Player Player1 = new Player();
 
+        List<String> availableClassesList = List.of("Warrior", "Mage", "Archer", "Assassin");
+        List<String> availableStarterWeaponsList = List.of("Wooden Sword", "Wooden Staff", "Wooden Bow", "Wooden Shuriken");
         System.out.println("Welcome to the main method of Java Game System!");
-        System.out.println("What do you want to know about?");
-        String[] availableClasses = {"Player", "NPCs", "Items"};
-        for (String availableClass : availableClasses) {
-            System.out.println("-" + availableClass);
-        }
+        System.out.println("Here, you will get to set your character up.");
 
         Scanner Input = new Scanner(System.in);
-        System.out.print("Input: ");
-        String query = Input.nextLine();
-        if (query.equalsIgnoreCase("Player")) {
-            System.out.println("Name: " + Markus.getPlayerName());
-            System.out.println("Equipped Weapon: " + Markus.getEquippedWeapon());
-            System.out.println("Health: " + Markus.getHealth());
-            System.out.println("Wisdom: " + Markus.getWisdom());
-            System.out.println("Strength: " + Markus.getStrength());
-            System.out.println("Speed: " + Markus.getSpeed());
+        System.out.print("Give your character a name: ");
+        String name = Input.nextLine();
+        Player1.setPlayerName(name);
+
+        System.out.println("Select your Character's class from the following options:");
+        for (String i : availableClassesList) {
+            System.out.println("-" + i);
+        }
+        while (true) {
+            System.out.println("Select a class: ");
+            String playerClass = Input.nextLine();
+            if (availableClassesList.contains(playerClass)) {
+                Player1.setPlayerClass(playerClass);
+                break;
+            } else {
+                System.out.println("Please choose a valid class.");
+            }
         }
 
+        System.out.println("Select your Character's starting weapon from the following options:");
+        for (String i : availableStarterWeaponsList) {
+            System.out.println("-" + i);
+        }
+        while (true) {
+            System.out.println("Select a starting weapon: ");
+            String startingWeapon = Input.nextLine();
+            if (availableStarterWeaponsList.contains(startingWeapon)) {
+                Player1.setEquippedWeapon(startingWeapon);
+                break;
+            } else {
+                System.out.println("Please choose a valid starting weapon.");
+            }
+        }
+
+        System.out.println("Your Characters Data:");
+        System.out.println("Name: " + Player1.getPlayerName());
+        System.out.println("Class: " + Player1.getPlayerClass());
+        System.out.println("Equipped Weapon: " + Player1.getEquippedWeapon());
+        System.out.println("Health: " + Player1.getPlayerHealth());
         Input.close();
     }
 }
