0f9a7217c3903b3dafb65d8807bbf1af1f4c2c99
Moritisimor <moritzgierschner@protonmail.com>
2025-05-28 18:52:09 +0200

    Started work on TierRegister Project. A program which allows you to document animals. cuz why not

---
+++ JavaProgramme/TierRegister/Main.java
@@ -0,0 +1,7 @@
+
+
+public class TierRegister {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ JavaProgramme/TierRegister/Tier.java
@@ -0,0 +1,49 @@
+public abstract class Tier {
+	private String name;
+	private String habitat;
+	private String farbe;
+	private String nahrung;
+	private Integer anzahlBeine;
+	private Boolean dichtesFell;
+	public abstract void geraeusch();
+
+	// GETTER SETTER REGION
+	
+	public String getName() {
+		return name;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public String getFarbe() {
+		return farbe;
+	}
+	public String getNahrung() {
+	       return nahrung;	
+	}
+	public int getAnzahlBeine() {
+		return anzahlBeine;
+	}
+	public boolean getDichtesFell() {
+		return dichtesFell;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+	public void setNahrung(String nahrung) {
+		this.nahrung = nahrung;
+	}
+	public void setAnzahlBeine(int anzahlBeine) {
+		this.anzahlBeine = anzahlBeine;
+	}
+	public void setDichtesFell(boolean dichtesFell) {
+		this.dichtesFell = dichtesFell;
+	}
+}
